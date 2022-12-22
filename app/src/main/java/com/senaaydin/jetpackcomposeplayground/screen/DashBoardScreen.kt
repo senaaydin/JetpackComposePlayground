6caaf67603c10a0c154a863aa4b335b4c865d269
Sena Aydın <senanuraydin@gmail.com>
2022-12-22 11:17:29 +0300

    clear imports

--- app/src/main/java/com/senaaydin/jetpackcomposeplayground/screen/DashBoardScreen.kt
+++ app/src/main/java/com/senaaydin/jetpackcomposeplayground/screen/DashBoardScreen.kt
@@ -3,13 +3,11 @@ package com.senaaydin.jetpackcomposeplayground.screen
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,7 +15,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.senaaydin.jetpackcomposeplayground.R
 import com.senaaydin.jetpackcomposeplayground.components.buttons.CustomElevatedButton
-import com.senaaydin.jetpackcomposeplayground.components.textfields.CustomTextField
 import com.senaaydin.jetpackcomposeplayground.layout.CustomScaffold
 import com.senaaydin.jetpackcomposeplayground.layout.CustomTopBar
 
